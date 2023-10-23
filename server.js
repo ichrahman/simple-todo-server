6935f0eb37ff876e0569ca65dd590c7c275035e1
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2023-10-23 14:45:19 +0600

    completed

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+require('dotenv').config()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tbgmean.mongodb.net/TODO?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const todosCollection = client.db('todos').collection('todo')
+
+
+        app.get('/todo', async (req, res) => {
+            const result = await todosCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        app.post('/todo', async (req, res) => {
+            const todo = req.body
+            todo.done = false
+            // console.log(todo)
+            const result = await todosCollection.insertOne(todo)
+            res.send(result)
+        })
+
+        app.patch('/todo/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const todo = await todosCollection.findOne(query)
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    done: !(todo.done)
+                }
+            }
+
+            const result = await todosCollection.updateOne(filter, updateDoc)
+
+            res.send(result)
+
+
+
+        })
+
+        app.delete('/todo/:id', async (req, res) => {
+            const id = req.params.id
+            // console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await todosCollection.deleteOne(query)
+            res.send(result)
+
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Todo World!')
+})
+
+app.listen(port, () => {
+    console.log(`Todo listening on port ${port}`)
+})
