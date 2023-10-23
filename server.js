34d6aa080a9ed558350dfcc79459c682063de6b9
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2023-10-23 14:55:01 +0600

    port updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = proce.env.PORT || 3000;
 require('dotenv').config()
 
 // middleware
